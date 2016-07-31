f088597b723f4df320b959947c9700194ebd443f
Yan Jiang <j582033@163.com>
2016-07-31 22:16:05 +0800

    add class TrueFalse

---
+++ GeoQuiz/app/src/main/java/com/example/xinjianzhang/geoquiz/TrueFalse.java
@@ -0,0 +1,43 @@
+package com.example.xinjianzhang.geoquiz;
+
+/**
+ * Created by xinjianzhang on 16/7/31.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TureFalse(int question, boolean tureQuestion) {
+        mQuestion = question;
+        mTrueQuestion = tureQuestion;
+    }
+
+    public int getmQuestion() {
+        return mQuestion;
+    }
+
+    public void setmQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean ismTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setmTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+
+    //    public int getmQuestion() {
+//        return mQuestion;
+//    }
+//    public void setmQuestion(int question) {
+//        mQuestion = question;
+//    }
+//    public boolean ismTrueQuestion {
+//        return mTrueQuestion;
+//    }
+//    public void setIsmTrueQuestion(boolean trueQuestion) {
+//        mTrueQuestion = trueQuestion;
+//    }
+}
