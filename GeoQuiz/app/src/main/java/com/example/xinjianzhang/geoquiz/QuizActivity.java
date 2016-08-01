fcc2d78e199c8671fa96c692fe40324775a503c0
Yan Jiang <j582033@163.com>
2016-08-01 22:46:41 +0800

    the GeoQuiz proj v2

--- GeoQuiz/app/src/main/java/com/example/xinjianzhang/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/xinjianzhang/geoquiz/QuizActivity.java
@@ -10,12 +10,46 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
 
+    //建立Question对象数组mQuestionBank, 该数组的所有元素都是一个Question对象（或类），然后用该类的构造方法创建实例
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+    //该数组的索引变量
+    private int mCurrentIndex = 0;
+
+    //单独封装问题更新部分代码，以便复用。
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    //检查用户的答案与问题的标准答案是否一致，并反馈toast消息给用户
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +67,20 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        //初始化显示第一个问题
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+
+
         //add by zhangxinjian
         mTrueButton = (Button)findViewById(R.id.ture_button);
         //使用匿名内部类实现OnClickListener()接口
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //does nothing yet but soon!
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(QuizActivity.this,
+//                        R.string.incorrect_toast,
+//                        Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
@@ -51,12 +89,26 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //does nothing yet but soon!
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(QuizActivity.this,
+//                        R.string.correct_toast,
+//                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
+
             }
         });
+
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //点击NEXT按钮时，显示下一道问题
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+
     }
 
     @Override
